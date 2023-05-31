96168afb5d7146e218e296c547f15d1837c088ee
KeadenM <keadenmeacham@gmail.com>
2023-05-30 21:05:33 -0600

    added helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -12,3 +12,19 @@ module.exports = {
     return `<span for="img" aria-label="book">${book}</span>`;
   },
 };
+// Formatting currency.
+function formatCurrency(amount) {
+  return amount.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+}
+
+function calculateTotal(number) {
+  return numbers.reduce ((total, num) => total + num, 0)
+}
+
+function formatNumber(number) {
+  return number.toLocaleString('en-US');
+}
+
+function calculateMonthlySavings(income, expenses) {
+  return income - expenses;
+}
