a55b9b73095d76a35dcd1e1af1c6462b0bf284d9
DLindquist <dannyl2131@gmail.com>
2023-06-01 20:27:59 -0600

    fixed signup and login routes

--- public/js/login.js
+++ public/js/login.js
@@ -3,7 +3,7 @@ const loginFormHandler = async (event) => {
 
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
-
+  console.log('login form hit')
   if (email && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
@@ -21,18 +21,19 @@ const loginFormHandler = async (event) => {
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
-
+  console.log("signup route hit");
   const username = document.querySelector('#username-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-
-  if (username && email && password) {
-    const response = await fetch('/api/users', {
+  const income = parseInt(document.querySelector('#user-income').value.trim());
+  
+  if (username && email && password && income) {
+    const response = await fetch('/api/users/', {
       method: 'POST',
-      body: JSON.stringify({ username, email, password }),
+      body: JSON.stringify({ username, email, password, income }),
       headers: { 'Content-Type': 'application/json' },
     });
-
+    
     if (response.ok) {
       document.location.replace('/');
     } else {
